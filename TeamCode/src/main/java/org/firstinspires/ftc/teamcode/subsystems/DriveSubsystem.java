60ceb7ca05b1fe19d1bdc7107d4cacdf00bb03f2
twelthDd <noahlaforce25@gmail.com>
2025-09-03 22:23:31 -0400

    Added Field Oriented Control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import androidx.annotation.NonNull;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class DriveSubsystem {
-
+    private static IMU gyro;
     static DcMotor left_front_motor;
     static DcMotor left_back_motor;
     static DcMotor right_front_motor;
@@ -15,11 +21,21 @@ public class DriveSubsystem {
     static Boolean FieldOriented = false;
 
 
-    public static void initialize(HardwareMap hwMap) {
+    public static void initialize(@NonNull HardwareMap hwMap) {
         left_front_motor = hwMap.get(DcMotor.class, "left_front_motor");
         right_front_motor = hwMap.get(DcMotor.class, "right_front_motor");
         left_back_motor = hwMap.get(DcMotor.class, "left_back_motor");
         right_back_motor = hwMap.get(DcMotor.class, "right_back_motor");
+        gyro = hwMap.get(IMU.class, "gyro");
+        gyro.initialize(
+                new IMU.Parameters(
+                        new RevHubOrientationOnRobot(
+                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+                        )
+                )
+        );
+
         if (!reverseDirections) {
             right_front_motor.setDirection(DcMotorSimple.Direction.REVERSE);
             right_back_motor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -34,16 +50,43 @@ public class DriveSubsystem {
         double drive  = gamepad.left_stick_y;
         double strafe = gamepad.left_stick_x;
         double twist  = gamepad.right_stick_x;
+        double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(twist), 1);
 
         if (!FieldOriented) {
             //robot-oriented
-            left_front_motor.setPower(drive + strafe + twist);
-            right_front_motor.setPower(drive - strafe - twist);
-            left_back_motor.setPower(drive - strafe + twist);
-            right_back_motor.setPower(drive + strafe - twist);
+            double left_front_power = (drive + strafe + twist) / denominator;
+            double right_front_power = (drive - strafe - twist) / denominator;
+            double left_back_power = (drive - strafe + twist) / denominator;
+            double right_back_power = (drive + strafe - twist) / denominator;
+
+            left_front_motor.setPower(left_front_power);
+            right_front_motor.setPower(right_front_power);
+            left_back_motor.setPower(left_back_power);
+            right_back_motor.setPower((right_back_power));
         } else {
             //field-oriented
-            //! TO BE IMPLEMENTED WHEN NOUR SENDS ME THE CODE
+            double botHeading = getHeading();
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = drive * Math.cos(-botHeading) - strafe * Math.sin(-botHeading);
+            double rotY = drive * Math.sin(-botHeading) + strafe * Math.cos(-botHeading);
+            double frontLeftPower = (rotY + rotX + twist) / denominator;
+            double backLeftPower = (rotY - rotX + twist) / denominator;
+            double frontRightPower = (rotY - rotX - twist) / denominator;
+            double backRightPower = (rotY + rotX - twist) / denominator;
+            // set the power of the motors
+            left_front_motor.setPower(frontLeftPower);
+            left_back_motor.setPower(backLeftPower);
+            right_front_motor.setPower(frontRightPower);
+            right_back_motor.setPower(backRightPower);
+
         }
+    }
+
+    public static double getHeading() {
+        return gyro.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    public void resetHeading() {
+        gyro.resetYaw();
     }
 }
