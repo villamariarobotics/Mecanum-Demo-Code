b2547e47c15693401672768aaf9e1b1fe0a301c0
TwelthDd <79723260+twelthDd@users.noreply.github.com>
2025-09-13 12:34:59 -0700

    Dashboard Gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleopMode.java
@@ -1,10 +1,20 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.bylazar.gamepad.GamepadManager;
+import com.bylazar.gamepad.PanelsGamepad;
+import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
+@TeleOp(name = "mainTeleop")
 public class MainTeleopMode extends OpMode {
+    TelemetryManager panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+    // Assuming com.bylazar.gamepad.PanelsGamepad is for managers
+    GamepadManager g1 = PanelsGamepad.INSTANCE.getFirstManager();
+    GamepadManager g2 = PanelsGamepad.INSTANCE.getSecondManager();
 
     DriveSubsystem drive_base = new DriveSubsystem();
 
@@ -16,6 +26,70 @@ public class MainTeleopMode extends OpMode {
     @Override
     public void loop() {
         drive_base.handleControllerInput(gamepad1);
-        drive_base.updateTelemetry();
+//        drive_base.updateTelemetry();
+        if (gamepad1.backWasPressed()) {
+            drive_base.resetHeading();
+        }
+        updateGamepadTelemetry();
+    }
+
+    private void updateGamepadTelemetry() {
+        Gamepad g11 = g1.asCombinedFTCGamepad(gamepad1);
+        Gamepad g22 = g2.asCombinedFTCGamepad(gamepad2);
+
+        panelsTelemetry.addLine("==== Buttons ====");
+        panelsTelemetry.addLine("A: " + g11.a);
+        panelsTelemetry.addLine("B: " + g11.b);
+        panelsTelemetry.addLine("X: " + g11.x);
+        panelsTelemetry.addLine("Y: " + g11.y);
+        panelsTelemetry.addLine("DPad Up: " + g11.dpad_up);
+        panelsTelemetry.addLine("DPad Down: " + g11.dpad_down);
+        panelsTelemetry.addLine("DPad Left: " + g11.dpad_left);
+        panelsTelemetry.addLine("DPad Right: " + g11.dpad_right);
+        panelsTelemetry.addLine("Left Bumper: " + g11.left_bumper);
+        panelsTelemetry.addLine("Right Bumper: " + g11.right_bumper);
+        panelsTelemetry.addLine("Left Trigger: " + g11.left_trigger);
+        panelsTelemetry.addLine("Right Trigger: " + g11.right_trigger);
+        panelsTelemetry.addLine("Start / Options: " + g11.options);
+        panelsTelemetry.addLine("Back / Share: " + g11.back);
+        panelsTelemetry.addLine("Guide / PS: " + g11.guide);
+        panelsTelemetry.addLine("Touchpad: " + g11.touchpad);
+        panelsTelemetry.addLine("Left Stick Button: " + g11.left_stick_button);
+        panelsTelemetry.addLine("Right Stick Button: " + g11.right_stick_button);
+
+        panelsTelemetry.addLine("==== Sticks ====");
+        panelsTelemetry.addLine("Left Stick X: " + g11.left_stick_x);
+        panelsTelemetry.addLine("Left Stick Y: " + g11.left_stick_y);
+        panelsTelemetry.addLine("Right Stick X: " + g11.right_stick_x);
+        panelsTelemetry.addLine("Right Stick Y: " + g11.right_stick_y);
+
+        panelsTelemetry.addLine("==== Buttons ====");
+        panelsTelemetry.addLine("A: " + g22.a);
+        panelsTelemetry.addLine("B: " + g22.b);
+        panelsTelemetry.addLine("X: " + g22.x);
+        panelsTelemetry.addLine("Y: " + g22.y);
+        panelsTelemetry.addLine("DPad Up: " + g22.dpad_up);
+        panelsTelemetry.addLine("DPad Down: " + g22.dpad_down);
+        panelsTelemetry.addLine("DPad Left: " + g22.dpad_left);
+        panelsTelemetry.addLine("DPad Right: " + g22.dpad_right);
+        panelsTelemetry.addLine("Left Bumper: " + g22.left_bumper);
+        panelsTelemetry.addLine("Right Bumper: " + g22.right_bumper);
+        panelsTelemetry.addLine("Left Trigger: " + g22.left_trigger);
+        panelsTelemetry.addLine("Right Trigger: " + g22.right_trigger);
+        panelsTelemetry.addLine("Start / Options: " + g22.options);
+        panelsTelemetry.addLine("Back / Share: " + g22.back);
+        panelsTelemetry.addLine("Guide / PS: " + g22.guide);
+        panelsTelemetry.addLine("Touchpad: " + g22.touchpad);
+        panelsTelemetry.addLine("Left Stick Button: " + g22.left_stick_button);
+        panelsTelemetry.addLine("Right Stick Button: " + g22.right_stick_button);
+
+        panelsTelemetry.addLine("==== Sticks ====");
+        panelsTelemetry.addLine("Left Stick X: " + g22.left_stick_x);
+        panelsTelemetry.addLine("Left Stick Y: " + g22.left_stick_y);
+        panelsTelemetry.addLine("Right Stick X: " + g22.right_stick_x);
+        panelsTelemetry.addLine("Right Stick Y: " + g22.right_stick_y);
+
+        panelsTelemetry.update(telemetry);
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -6,9 +6,7 @@ import static org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem.DriveCons
 
 //import androidx.annotation.NonNull;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -19,25 +17,24 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
-public class DriveSubsystem {
+public class
+DriveSubsystem {
     private static IMU gyro;
     static DcMotor left_front_motor;
     static DcMotor left_back_motor;
     static DcMotor right_front_motor;
     static DcMotor right_back_motor;
 
-//    @Config
-public static class DriveConstants {
-    static Boolean reverseDirections = false;
-    static Boolean FieldOriented = false;
-
-
-}
+    @Configurable
+    public static class DriveConstants {
+        public static Boolean reverseDirections = true;
+        public static Boolean FieldOriented = true;
+       public static double SpeedModifier = 0.75;
+    }
 
 
 
     public static void initialize(HardwareMap hwMap) {
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         left_front_motor = hwMap.get(DcMotor.class, "left_front_motor");
         right_front_motor = hwMap.get(DcMotor.class, "right_front_motor");
         left_back_motor = hwMap.get(DcMotor.class, "left_back_motor");
@@ -61,19 +58,19 @@ public static class DriveConstants {
         }
     }
     public void handleControllerInput(Gamepad gamepad) {
-        // Mecanum drive is controlled with three axes: drive (front-and-back),
+        // Mecanum drive i controlled with three axes: drive (front-and-back),
         // strafe (left-and-right), and twist (rotating the whole chassis).
-        double drive  = gamepad.left_stick_y;
-        double strafe = gamepad.left_stick_x;
-        double twist  = gamepad.right_stick_x;
+        double drive  = -gamepad.left_stick_x;
+        double strafe = gamepad.left_stick_y;
+        double twist  = -gamepad.right_stick_x;
         double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(twist), 1);
 
         if (!FieldOriented) {
             //robot-oriented
-            double left_front_power = (drive + strafe + twist) / denominator;
-            double right_front_power = (drive - strafe - twist) / denominator;
-            double left_back_power = (drive - strafe + twist) / denominator;
-            double right_back_power = (drive + strafe - twist) / denominator;
+            double left_front_power = DriveConstants.SpeedModifier * (drive + strafe + twist) / denominator;
+            double right_front_power = DriveConstants.SpeedModifier * (drive - strafe - twist) / denominator;
+            double left_back_power = DriveConstants.SpeedModifier * (drive - strafe + twist) / denominator;
+            double right_back_power = DriveConstants.SpeedModifier * (drive + strafe - twist) / denominator;
 
             left_front_motor.setPower(left_front_power);
             right_front_motor.setPower(right_front_power);
@@ -85,10 +82,10 @@ public static class DriveConstants {
             // Rotate the movement direction counter to the bot's rotation
             double rotX = drive * Math.cos(-botHeading) - strafe * Math.sin(-botHeading);
             double rotY = drive * Math.sin(-botHeading) + strafe * Math.cos(-botHeading);
-            double frontLeftPower = (rotY + rotX + twist) / denominator;
-            double backLeftPower = (rotY - rotX + twist) / denominator;
-            double frontRightPower = (rotY - rotX - twist) / denominator;
-            double backRightPower = (rotY + rotX - twist) / denominator;
+            double frontLeftPower = DriveConstants.SpeedModifier * (rotY + rotX + twist) / denominator;
+            double backLeftPower = DriveConstants.SpeedModifier * (rotY - rotX + twist) / denominator;
+            double frontRightPower = DriveConstants.SpeedModifier * (rotY - rotX - twist) / denominator;
+            double backRightPower = DriveConstants.SpeedModifier * (rotY + rotX - twist) / denominator;
             // set the power of the motors
             left_front_motor.setPower(frontLeftPower);
             left_back_motor.setPower(backLeftPower);
@@ -99,12 +96,6 @@ public static class DriveConstants {
     }
 
     public void updateTelemetry() {
-        telemetry.addData("Heading (deg)", getHeading());
-        telemetry.addData("Left Front Power", left_front_motor.getPower());
-        telemetry.addData("Left Back Power", left_back_motor.getPower());
-        telemetry.addData("Right Front Power", right_front_motor.getPower());
-        telemetry.addData("Right Back Power", right_back_motor.getPower());
-        telemetry.update();
     }
 
     public static double getHeading() {
